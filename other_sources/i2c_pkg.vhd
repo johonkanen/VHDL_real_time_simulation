89fb59044d809e521e7fc531159c379edffa3d30
jari <jaarhonka@yahoo.com>
2023-03-03 23:25:55 +0200

    savegame

--- other_sources/i2c_pkg.vhd
+++ other_sources/i2c_pkg.vhd
@@ -18,37 +18,90 @@ package i2c_pkg is
 
     subtype read_and_write_registers is integer range 0 to 80;
     subtype read_only_registers is integer range 96 to 127;
+------------------------------------------------------------------------
+    type i2c_record is record
+        clock                   : std_logic                 ;
+        data                    : std_logic                 ;
+        clock_counter           : integer range 0 to 2**10-1;
+        bit_counter             : integer range 0 to 31;
+        transmit_shift_register : std_logic_vector(31 downto 0);
+        receive_shift_register  : std_logic_vector(31 downto 0);
+    end record;
+
+    constant init_i2c : i2c_record := ('1', '1', clock_divider_max, 0, (others => '0'), (others => '0'));
+------------------------------------------------------------------------
+    procedure create_i2c (
+        signal self : inout i2c_record;
+        signal clock_out : out std_logic);
 
     procedure create_i2c (
         signal counter : inout integer;
         counter_max : integer;
         signal clock_out : out std_logic );
-
+------------------------------------------------------------------------
 end package i2c_pkg;
 
 package body i2c_pkg is
 ------------------------------------------------------------------------
-        procedure create_i2c
-        (
-            signal counter : inout integer;
-            counter_max : integer;
-            signal clock_out : out std_logic 
-        ) is
-        begin
-            if counter > 0 then
-                counter <= counter - 1;
-            else
-                counter <= counter_max;
-            end if;
-
-            if counter > counter_max/2 then
-                clock_out <= '1';
-            else
-                clock_out <= '0';
-            end if;
-            
-        end create_i2c;
+    procedure create_i2c
+    (
+        signal self : inout i2c_record;
+        signal clock_out : out std_logic
+    ) is
+    impure function i2c_clock_falling_edge return boolean is
+    begin
+        return self.clock_counter = clock_divider_max/2;
+        
+    end i2c_clock_falling_edge;
     --------------------------------------------------
+    begin
+        create_i2c(self.clock_counter, clock_divider_max, clock_out);
+        if i2c_clock_falling_edge and self.bit_counter > 0 then
+            self.transmit_shift_register <= self.transmit_shift_register(30 downto 0) & '0';
+            self.bit_counter <= self.bit_counter - 1;
+        end if;
+    end create_i2c;
+------------------------------------------------------------------------
+    procedure create_i2c
+    (
+        signal counter : inout integer;
+        counter_max : integer;
+        signal clock_out : out std_logic 
+    ) is
+    --------------------------------------------------
+    begin
+        if counter > 0 then
+            counter <= counter - 1;
+        else
+            counter <= counter_max;
+        end if;
+
+        if counter > counter_max/2 then
+            clock_out <= '1';
+        else
+            clock_out <= '0';
+        end if;
+        
+    end create_i2c;
+--------------------------------------------------
+    procedure write_data_to_i2c_address
+    (
+        signal i2c_object : out i2c_record;
+        address : in integer;
+        data : in std_logic_vector(7 downto 0)
+    ) is
+    begin
+        
+    end write_data_to_i2c_address;
+--------------------------------------------------
+    procedure read_i2c_address
+    (
+        signal i2c_object : out i2c_record;
+        address : in integer
+    ) is
+    begin
+        
+    end read_i2c_address;
+--------------------------------------------------
 
 end package body i2c_pkg;
-
--- testbenches/i2c/i2c_tb.vhd
+++ testbenches/i2c/i2c_tb.vhd
@@ -15,25 +15,29 @@ end;
 architecture vunit_simulation of i2c_tb is
 
     constant clock_period      : time    := 8.333 ns;
-    constant simtime_in_clocks : integer := 5000;
+    constant simtime_in_clocks : integer := 15000;
     
     signal simulator_clock     : std_logic := '0';
     signal simulation_counter  : natural   := 0;
     -----------------------------------
     -- simulation specific signals ----
-    signal i2c_clock : std_logic := '1';
-    signal i2c_data : std_logic := '1';
 
+    signal i2c : i2c_record := init_i2c;
+
+    signal i2c_clock         : std_logic                  := '1';
+    signal i2c_data          : std_logic                  := '1';
     signal i2c_clock_counter : integer range 0 to 2**10-1 := clock_divider_max;
-
-    signal transmit_shift_register : std_logic_vector(15 downto 0) := (others => '1');
-    signal receive_shift_register  : std_logic_vector(15 downto 0) := (others => '1');
 
     constant start_frame : std_logic := '0';
     constant stop_frame : std_logic := '1';
 
     constant data_address : std_logic_vector(7 downto 0) := "00000000";
     signal a_frame : std_logic_vector(27 downto 0) := start_frame & i2c_address_0x20 & write_with_1 & acknowledge & data_address & acknowledge & "00000000" & stop_frame;
+
+    signal transmit_shift_register : std_logic_vector(15 downto 0) := a_frame(27 downto 27-15);
+    signal receive_shift_register  : std_logic_vector(15 downto 0) := (others => '1');
+
+
 
     signal i2c_direction_is_write_when_1 : std_logic := '1';
 
@@ -65,7 +69,9 @@ begin
         
     begin
         if rising_edge(i2c_clock) then
-            ma12070_receive_register <= ma12070_receive_register(ma12070_receive_register'left - 1 downto 0) & i2c_data;
+            if ma12070_receive_register /= a_frame(27 downto 27-15) then
+                ma12070_receive_register <= ma12070_receive_register(ma12070_receive_register'left - 1 downto 0) & i2c_data;
+            end if;
         end if; --rising_edge
     end process ma12070;	
 ------------------------------------------------------------------------
@@ -86,7 +92,7 @@ begin
 
             create_i2c(i2c_clock_counter, clock_divider_max, i2c_clock);
 
-            if i2c_clock_counter = clock_divider_max/2 then
+            if i2c_clock_falling_edge then
                 transmit_shift_register <= transmit_shift_register(transmit_shift_register'left -1 downto 0) & '0';
             end if;
 
