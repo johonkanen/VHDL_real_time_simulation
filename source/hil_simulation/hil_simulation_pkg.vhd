2e9c11b6900d8a97322c6327120ec00e54e093a2
jari <jaarhonka@yahoo.com>
2023-03-08 01:10:34 +0200

    remove unnecessary packages

--- source/hil_simulation/hil_simulation_pkg.vhd
+++ source/hil_simulation/hil_simulation_pkg.vhd
@@ -3,9 +3,7 @@ library ieee;
     use ieee.numeric_std.all;
 
     use work.fpga_interconnect_pkg.all;
-    use work.multiplier_pkg.all;
     use work.lcr_filter_model_pkg.all;
-    use work.real_to_fixed_pkg.all;
     use work.filtered_buck_model_pkg.all;
 
 entity hil_simulation is
@@ -19,7 +17,7 @@ end entity hil_simulation;
 
 architecture rtl of hil_simulation is
 
-    signal m : filtered_buck_record := init_filtered_buck;
+    signal filtered_buck : filtered_buck_record := init_filtered_buck;
 
     signal output_voltage   : real := 0.0;
     signal inductor_current : real := 0.0;
@@ -39,18 +37,18 @@ begin
             init_bus(bus_from_hil_simulator);
             connect_data_to_address(bus_to_hil_simulator           , bus_from_hil_simulator , 1000 , input_voltage);
             connect_data_to_address(bus_to_hil_simulator           , bus_from_hil_simulator , 1001 , load_current);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1002 , get_inductor_current(m.input_lc1)  / 2**7+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1003 , get_capacitor_voltage(m.input_lc1) / 2**10+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1004 , get_inductor_current(m.input_lc2)  / 2**7+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1005 , get_capacitor_voltage(m.input_lc2) / 2**10+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1006 , get_inductor_current(m.primary_lc)  / 2**7+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1007 , get_capacitor_voltage(m.primary_lc) / 2**10+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1008 , get_inductor_current(m.output_lc1)  / 2**7+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1009 , get_capacitor_voltage(m.output_lc1) / 2**10+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1010 , get_inductor_current(m.output_lc2)  / 2**7+32768);
-            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1011 , get_capacitor_voltage(m.output_lc2) / 2**10+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1002 , get_inductor_current(filtered_buck.input_lc1)  / 2**7+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1003 , get_capacitor_voltage(filtered_buck.input_lc1) / 2**10+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1004 , get_inductor_current(filtered_buck.input_lc2)  / 2**7+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1005 , get_capacitor_voltage(filtered_buck.input_lc2) / 2**10+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1006 , get_inductor_current(filtered_buck.primary_lc)  / 2**7+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1007 , get_capacitor_voltage(filtered_buck.primary_lc) / 2**10+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1008 , get_inductor_current(filtered_buck.output_lc1)  / 2**7+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1009 , get_capacitor_voltage(filtered_buck.output_lc1) / 2**10+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1010 , get_inductor_current(filtered_buck.output_lc2)  / 2**7+32768);
+            connect_read_only_data_to_address(bus_to_hil_simulator , bus_from_hil_simulator , 1011 , get_capacitor_voltage(filtered_buck.output_lc2) / 2**10+32768);
 
-            create_filtered_buck(m, input_voltage*2**10, load_current*2**7);
+            create_filtered_buck(filtered_buck, input_voltage*2**10, load_current*2**7);
 
             if simulation_counter > 0 then
                 simulation_counter <= simulation_counter - 1;
@@ -59,7 +57,7 @@ begin
             end if;
 
             if simulation_counter = 0 then
-                request_filtered_buck_calculation(m );
+                request_filtered_buck_calculation(filtered_buck);
             end if;
 
         end if; --rising_edge
