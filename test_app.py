cf1e3f7fd27730837aaeb7661b69327d3922ef14
Jari Honkanen <jaarhonka@yahoo.com>
2023-03-21 00:50:05 +0200

    figures taken with this

--- test_app.py
+++ test_app.py
@@ -30,13 +30,6 @@ def exitation():
     uart.write_data_to_address(1001, 0);
     time.sleep(0.1)
 
-    # uart.write_data_to_address(1000, 12800);
-    # uart.write_data_to_address(1001, 0);
-    # time.sleep(0.1)
-    # uart.write_data_to_address(1012, int(25000))
-    # time.sleep(0.1)
-    # uart.write_data_to_address(1014, int(1500))
-
 def get_voltage(address):
 
     uart.request_data_stream_from_address(address, number_of_datapoints_to_stream)
@@ -52,31 +45,17 @@ def get_data(address):
 def get_current(address):
     return get_voltage(address)/8.0
 
-pyplot.subplot(4, 1, 1)
+pyplot.subplot(3, 1, 1)
 pyplot.plot(get_voltage(1007))
-pyplot.subplot(4, 1, 2)
-pyplot.plot(get_current(1004))
-pyplot.plot(get_current(1006))
-pyplot.plot(get_current(1015))
-pyplot.subplot(4, 1, 3)
-pyplot.plot(get_data(1012))
-pyplot.plot(-32768 + get_data(1013))
-pyplot.subplot(4, 1, 4)
 pyplot.plot(get_voltage(1005))
-# pyplot.plot(get_voltage(1005))
-# pyplot.legend(['input emi 1 voltage', 'input emi 2 voltage']) 
-# pyplot.subplot(2, 2, 3)
-# pyplot.plot(get_voltage(1007))
-# pyplot.plot(get_voltage(1009))
-# pyplot.plot(get_voltage(1011))
-# pyplot.legend(['primary lc voltage', 'output emi 1 voltage', 'output emi 2 voltage'])
-# pyplot.subplot(2, 2, 2)
-# pyplot.plot(get_current(1002))
+pyplot.plot(get_voltage(1011))
+pyplot.legend(['primary lc voltage', 'bridge input voltage', 'output voltage'])
+pyplot.subplot(3, 1, 2)
 # pyplot.plot(get_current(1004))
-# pyplot.legend(['input emi 1 current', 'input emi 2 current']) 
-# pyplot.subplot(2, 2, 4)
 # pyplot.plot(get_current(1006))
-# pyplot.plot(get_current(1008))
-# pyplot.plot(get_current(1010))
-# pyplot.legend(['primary lc current', 'output emi 1 current', 'output emi 2 current'])
+pyplot.plot((-32768 + get_data(1013))/32768)
+pyplot.legend(['input_voltage/output_voltage -feedforward term'])
+pyplot.subplot(3, 1, 3)
+pyplot.plot(get_data(1012)/32768)
+pyplot.legend(['duty ratio'])
 pyplot.show()
